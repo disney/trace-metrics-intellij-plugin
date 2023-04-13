8a08dfec26ba45f76292d029f2b242b5d50085e9
Daniel Kirkdorffer <dankirkd@comcast.net>
2023-04-13 13:52:11 -0700

    * Converted to use New Relic NerdGraph API for NRQL queries.  * Updating IDE compatibility to 2020.2.  * Updating various dependency versions.  * Renamed logo files to avoid possible conflicts with other plugins.

--- src/test/java/com/disney/idea/client/NewRelicClientTest.java
+++ src/test/java/com/disney/idea/client/NewRelicClientTest.java
@@ -11,7 +11,7 @@ import java.util.Map;
 
 import org.apache.http.HttpVersion;
 import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.message.BasicStatusLine;
@@ -20,6 +20,7 @@ import org.junit.Test;
 
 public class NewRelicClientTest {
 
+    private final String accountId = "fakeAccountId";
     private final String apiKey = "fakeApiKey";
     private final String nrUrl = "http://fakehost";
     private final String appName = "fakeAppName";
@@ -32,45 +33,46 @@ public class NewRelicClientTest {
     @Before
     public void setup() {
         httpClient = mock(CloseableHttpClient.class);
-        newRelicClient = new NewRelicClient(apiKey, nrUrl, appName, numDays, untilDate, httpClient);
+        newRelicClient = new NewRelicClient(accountId, apiKey, nrUrl, appName, numDays, untilDate, httpClient);
     }
 
     @Test
     public void query_retryOnceAfterException() throws Exception {
-        when(httpClient.execute(any(HttpGet.class))).thenThrow(new IOException("Failed"));
+        when(httpClient.execute(any(HttpPost.class))).thenThrow(new IOException("Failed"));
 
         Map<String, Long> result = newRelicClient.query(null);
 
         assert result.isEmpty();
-        verify(httpClient, times(2)).execute(any(HttpGet.class));
+        verify(httpClient, times(2)).execute(any(HttpPost.class));
     }
 
     @Test
     public void query_retryOnceAfterNon200Response() throws Exception {
         CloseableHttpResponse response = mock(CloseableHttpResponse.class);
 
-        when(httpClient.execute(any(HttpGet.class))).thenReturn(response);
+        when(httpClient.execute(any(HttpPost.class))).thenReturn(response);
         when(response.getStatusLine()).thenReturn(new BasicStatusLine(HttpVersion.HTTP_1_1, 500, null));
 
         Map<String, Long> result = newRelicClient.query(null);
 
         assert result.isEmpty();
-        verify(httpClient, times(2)).execute(any(HttpGet.class));
+        verify(httpClient, times(2)).execute(any(HttpPost.class));
     }
 
     @Test
     public void query_noRetriesAfterSuccess() throws Exception {
         CloseableHttpResponse response = mock(CloseableHttpResponse.class);
-        StringEntity entity = new StringEntity("{ \"facets\" : [ { \"name\": \"this\", \"results\": [ {\"count\": 5 } ] } ] }");
+        // {"data":{"actor":{"nrql":{"results":[{"facet":"...","count":...,"name":"..."}, { etc... }]}}}}
+        StringEntity entity = new StringEntity("{\"data\":{\"actor\":{\"nrql\":{\"results\":[{\"facet\":\"this\",\"count\":5,\"name\":\"this\"}]}}}}");
 
-        when(httpClient.execute(any(HttpGet.class))).thenReturn(response);
+        when(httpClient.execute(any(HttpPost.class))).thenReturn(response);
         when(response.getStatusLine()).thenReturn(new BasicStatusLine(HttpVersion.HTTP_1_1, 200, null));
         when(response.getEntity()).thenReturn(entity);
 
         Map<String, Long> result = newRelicClient.query(null);
 
         assert result.get("this") == 5;
-        verify(httpClient, times(1)).execute(any(HttpGet.class));
+        verify(httpClient, times(1)).execute(any(HttpPost.class));
 
     }
 }
